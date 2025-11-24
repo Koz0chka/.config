6b5924a10e980d78b25993b49e52f6ead2f7d8b3
Leonid-Yakovlev63 <j772115@gmail.com>
2025-11-24 22:53:24 +0400

    fish aliases

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+alias k="kubectl"
+alias m="minikube"
+alias vu="sudo wg-quick up netherlands"
+alias vd="sudo wg-quick down netherlands"
